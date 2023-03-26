11ecdd14d09a2da21cac7783dd02f4e638699514
muchubatactics <byooxmckeith@gmail.com>
2023-03-26 16:16:46 +0300

    just adding functionality

--- kilo.c
+++ kilo.c
@@ -1,6 +1,44 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
+struct termios orig_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode()
+{
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(IXON | ICRNL);
+    // raw.c_oflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
-    return 35;
+    enableRawMode();
+
+    char c;
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    {
+        if(iscntrl(c))
+        {
+            printf("%d\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\n", c, c);
+        }   
+    }
+    return 0;
 }
