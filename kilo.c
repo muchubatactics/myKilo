de233c11ae5b15e4c9c4eff75fc045a7f582255e
muchubatactics <byooxmckeith@gmail.com>
2023-03-29 21:47:34 +0300

    routine

--- kilo.c
+++ kilo.c
@@ -69,18 +69,37 @@ char editorReadKey()
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols)
+{
+    char buf[32];
+    unsigned int i = 0;
+
+    if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while(i < sizeof(buf) - 1)
+    {
+        if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        ++i;
+    }
+
+    buf[i] = '\0';
+    if(buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if(sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+
+}
+
 int getWindowSize(int *rows, int *cols)
 {
     struct winsize ws;
 
-    if(1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
     {
-        if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
-        {
-            return -1;
-        }
+        if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
         editorReadKey();
-        return -1;
+        return getCursorPosition(rows, cols);;
     }
     else
     {
@@ -90,6 +109,7 @@ int getWindowSize(int *rows, int *cols)
     }
 }
 
+
 /*** output ***/
 
 void editorDrawRows()
@@ -97,7 +117,12 @@ void editorDrawRows()
     int y;
     for(y = 0; y < E.screenrows; ++y)
     {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+
+        if(y < E.screenrows - 1)
+        {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 
@@ -149,4 +174,4 @@ int main()
     return 0;
 }
 
-/*START FROM WINDOW SIZE, THE HARD WAY*/
+/*START FROM APPEND BUFFER*/
