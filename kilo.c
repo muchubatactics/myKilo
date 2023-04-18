2ef305e482dc59ab439d951617512bea1a1c271f
muchubatactics <byooxmckeith@gmail.com>
2023-04-18 23:30:37 +0300

    routine

--- kilo.c
+++ kilo.c
@@ -67,6 +67,8 @@ enum editorKey
 
 /**** prototypes ****/
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 /***terminal***/
 void die(const char* s)
@@ -430,7 +432,15 @@ void editorOpen(char *filename)
 
 void editorSave()
 {
-    if (E.filename == NULL) return;
+    if (E.filename == NULL)
+    {
+        E.filename = editorPrompt("Save as: %s");
+        if (E.filename == NULL)
+        {
+            editorSetStatusMessage("Save Aborted!");
+            return;
+        }
+    }
 
     int len;
     char *buf = editorRowToString(&len);
@@ -621,6 +631,52 @@ void editorSetStatusMessage(const char *fmt, ...)
 
 /*** input ***/
 
+char *editorPrompt(char *prompt)
+{
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1)
+    {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == DEL_KEY || c == BACKSPACE || c == CTRL_KEY('h'))
+        {
+            if (buflen != 0) buf[--buflen] = '\0';
+        }
+        else if (c == '\x1b')
+        {
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        }
+        else if (c == '\r')
+        {
+            if(buflen != 0)
+            {
+                editorSetStatusMessage("");
+                return buf;
+            }
+        }
+        else if(!iscntrl(c) && c < 128)
+        {
+            if(buflen == bufsize - 1)
+            {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+
+}
+
 void editorMoveCursor(int key)
 {
     erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
@@ -790,4 +846,4 @@ int main(int argc, char **argv)
     return 0;
 }
 
-/*start from quit confirmation*/
+/*start from search*/
